dee3be15cb1b4e58dd0a2d226a7c8560a6e417f5
Vasanth261004 <vasanth.p2610@gmail.com>
2024-04-11 19:30:59 +0530

    modifie

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const cmt = document.getElementById('cmt')
 const shr = document.getElementById('shr')
 
 let cmtshow = document.querySelector('.cmt-pop-up')
+let shrshow = document.querySelector('.shr-pop-up')
 let close = document.querySelector('.btn-c')
 let post = document.querySelector('.btn-p')
+let formbox = document.querySelector('.form-box')
 
 let like_count = document.getElementById('like-count')
 let cmt_count = document.getElementById('cmt-count')
@@ -18,22 +20,28 @@ shr_count.textContent = shr_value;
 like_count.textContent = like_value;
 cmt_count.textContent = cmt_value;
 
-shr.addEventListener('click', function() {
-    shr_count.textContent = shr_value++;
-})
-
 like.addEventListener('click', function() {
     like_count.textContent = like_value++;
-})
+});
+
+cmt.addEventListener('click', () => {
+    cmtshow.classList.add('active');
+});
 
 post.addEventListener('click', () => {
     cmt_count.textContent = cmt_value++;
     cmtshow.classList.remove('active');
-})
+});
 
-cmt.addEventListener('click', () => {
-    cmtshow.classList.add('active');
-})
 close.addEventListener('click', () => {
     cmtshow.classList.remove('active');
-})
+});
+
+shr.addEventListener('click', () => {
+    shrshow.classList.add('active');
+});
+
+shrshow.addEventListener('click', () => {
+    shrshow.classList.remove('active');
+    shr_count.textContent = shr_value++;
+});
